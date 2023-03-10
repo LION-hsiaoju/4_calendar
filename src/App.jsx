f8050779551618d5c2c15a53195e69d3c8c38bf8
HsiaooooooJu <kelllllyko@gmail.com>
2023-03-10 17:28:54 +0800

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import Calendar from './calendar/Calendar'
+
+function App() {
+  return (
+    <div className='App'>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
